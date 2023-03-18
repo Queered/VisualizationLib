1b01b45b6a5419b68ec7bb74809b5569d15bf561
queered <121299278+Queered@users.noreply.github.com>
2023-03-19 01:53:40 +0300

    Create example.lua

---
+++ example.lua
@@ -0,0 +1,3 @@
+local Visualization = loadstring(game:HttpGet("https://raw.githubusercontent.com/Queered/VisualizationLib/main/lib.lua"))()
+
+Visualization.createButton("Visualize Player Position", nil, Visualization.visualizePlayerPosition)
